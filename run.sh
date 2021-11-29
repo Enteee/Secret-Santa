d57f4c6e56985fdc715dc81914285ce252b24b97
Ente <ducksource@duckpond.ch>
2021-11-29 01:49:24 +0100

    fix port

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ set -exuo pipefail
 SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 IMAGE="$(docker build -q "${SCRIPT_DIR}")"
 
-PORT="$(shuf -i 1024-65535 -n 1)"
+PORT="42418"
 
 docker run \
   -p "${PORT}:${PORT}" \
