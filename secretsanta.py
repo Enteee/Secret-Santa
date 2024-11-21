dfec2e9e876ca0c3335137f43e480f327fbff756
Ente <ducksource@duckpond.ch>
2024-11-22 00:26:41 +0100

    format code with black

--- secretsanta.py
+++ secretsanta.py
@@ -5,48 +5,30 @@ import random
 import secrets
 import string
 
-parser = argparse.ArgumentParser(description='Secret Santa')
+parser = argparse.ArgumentParser(description="Secret Santa")
 
 parser.add_argument(
-        'santas',
-        type=str,
-        nargs='+',
-        help='People participating in the secret santa scheme'
+    "santas",
+    type=str,
+    nargs="+",
+    help="People participating in the secret santa scheme",
 )
 
 parser.add_argument(
-        '--print_host',
-        type=str,
-        default="localhost",
-        help='Host to run webserver on',
+    "--print_host",
+    type=str,
+    default="localhost",
+    help="Host to run webserver on",
 )
 
-parser.add_argument(
-        '--port',
-        type=int,
-        default=80,
-        help='Port to run webserver on'
-)
+parser.add_argument("--port", type=int, default=80, help="Port to run webserver on")
+
+parser.add_argument("--tls-cert", type=str, default=None, help="TLS certificate path")
+
+parser.add_argument("--tls-key", type=str, default=None, help="TLS key path")
 
 parser.add_argument(
-        '--tls-cert',
-        type=str,
-        default=None,
-        help='TLS certificate path'
-)
-
-parser.add_argument(
-        '--tls-key',
-        type=str,
-        default=None,
-        help='TLS key path'
-)
-
-parser.add_argument(
-        '--seed',
-        type=int,
-        default=secrets.randbits(128),
-        help='Random seed'
+    "--seed", type=int, default=secrets.randbits(128), help="Random seed"
 )
 
 args = parser.parse_args()
@@ -55,28 +37,34 @@ args = parser.parse_args()
 if args.tls_cert is not None and args.tls_key is not None:
     tls_context = (args.tls_cert, args.tls_key)
 else:
-    tls_context = 'adhoc'
+    tls_context = "adhoc"
 
 app = Flask(__name__)
 
 secret_to_santa = {}
 
+
 @app.route("/")
 def root():
     return f"Santas: {args.santas}"
 
-@app.route('/<secret>')
+
+@app.route("/<secret>")
 def get_secret(secret):
     try:
         santa, dest = secret_to_santa[secret]
     except KeyError:
         abort(404, description="Secret was not found")
-    return f"<html><body><h1>{santa} du bist secret santa von: {dest}</h1></body></html>"
+    return (
+        f"<html><body><h1>{santa} du bist secret santa von: {dest}</h1></body></html>"
+    )
+
 
 def make_secret():
     alphabet = string.ascii_letters + string.digits
-    secret = ''.join(random.choice(alphabet) for i in range(15))
+    secret = "".join(random.choice(alphabet) for i in range(15))
     return secret
+
 
 def main():
     random.seed(args.seed)
@@ -88,8 +76,8 @@ def main():
     to_print = []
     for i, santa in enumerate(santas):
         secret = make_secret()
-        dst_santa = santas[ (i+1) % len(santas) ]
-        secret_to_santa[secret] = ( santa , dst_santa)
+        dst_santa = santas[(i + 1) % len(santas)]
+        secret_to_santa[secret] = (santa, dst_santa)
         to_print.append(f"{santa}: https://{args.print_host}:{args.port}/{secret}")
 
     # print after shuffling
@@ -103,6 +91,7 @@ def main():
         ssl_context=tls_context,
         port=args.port,
     )
+
 
 if __name__ == "__main__":
     main()
